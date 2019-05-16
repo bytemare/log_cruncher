40b754924b9611c1ee0a04d477f22973c33b6ab4
bytemare <{ID}+{username}@users.noreply.github.com>
2019-05-16 11:30:40 +0200

    changed variable name to be clearer

--- log_clean.sh
+++ log_clean.sh
@@ -14,13 +14,13 @@ target_file= $1
 newline_delimiter="\n"
 
 log_ext=".log"
-res_ext=".clean"
+new_file_extension=".clean"
 
 for fic in `ls *${log_ext}`
 do
     s=@fic.string
     strings ${fic} > ${s}
-    res=${fic}${res_ext}
-    awk 'BEGIN {accum_line = "";} /^</{if(length(accum_line)){print accum_line; accum_line = "";}} {accum_line = accum_line "" $0;} END {if(length(accum_line)){print accum_line; }}' ${s} > ${res}
+    res=${fic}${new_file_extension}
+    awk 'BEGIN {accum_line = "";} /^</{if(length(accum_line)){print accum_line; accum_line = "";}} {accum_line = accum_line "" $0;} END {if(length(accum_line)){print accum_line; }}' ${s} > ${new_file_extension}
     rm ${s} ${fic}
 done
